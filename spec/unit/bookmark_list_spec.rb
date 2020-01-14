e5a953cfc42a5dc0ff55bdf64f3c426df6ee91cf
benauld <ben@portobello-consultancy.co.uk>
2020-01-14 13:05:22 +0000

    hyperlinks

--- spec/unit/bookmark_list_spec.rb
+++ spec/unit/bookmark_list_spec.rb
@@ -1,11 +1,12 @@
 require 'bookmark_list'
+require 'bookmark'
 
 describe BookmarkList do
   let(:subject){BookmarkList.new(Bookmark)}
 
   describe "#view_bookmarks" do
     it "shows a list of the bookmarks" do
-      expect(subject.show_bookmarks).to include 'http://www.google.com'
+      expect(subject.show_bookmarks).to be_a_kind_of Array
     end
   end
 end
